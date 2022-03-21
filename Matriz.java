ddc5d3db1cd60f9e75e9093a09f54cc673dbe92b
Gustavo Cassemiro Silva <gustavo-santosfc@hotmail.com>
2022-03-21 17:23:51 +0000

    Matriz

---
+++ Matriz.java
@@ -0,0 +1,16 @@
+import java.util.Sc
+public class Matriz 
+{
+    public static void main(String[] args) 
+    {
+        int[] idades;
+    idades = new int[10];
+    idades [4] = 22;
+    Scanner sc=new Scanner(System.in);
+    for (int index = 0; index < idades.length; index++)
+
+    System.out.println("Eu tenho " +idades[index]+" anos");
+
+    }
+
+}
